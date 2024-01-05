56dded3adef37cf3dd9ca39e3b895060fb7732c0
Lorenzo Ceccanti <133343893+lorenzoceccanti@users.noreply.github.com>
2024-01-05 17:39:17 +0100

    Delete startApp.sh

--- startApp.sh
+++
@@ -1,6 +0,0 @@
-cd SafeDomoticHome
-mvn clean install
-mvn package
-read -p "Compilation done. Press enter to execute"
-gnome-terminal -- sh -c "java -jar target/SafeDomoticHome-0.0.1-SNAPSHOT.jar; exec bash"
-
